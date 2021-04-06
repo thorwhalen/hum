d06d66ce1740ba1d694d6e3171e96b95fb5d442a
Thor Whalen <thorwhalen1@gmail.com>
2021-04-06 14:44:49 -0700

    ModuleNotFoundIgnore -> contextlib.suppress(ModuleNotFoundError)

--- hum/gen/__init__.py
+++ hum/gen/__init__.py
@@ -1,17 +1,19 @@
-from hum.util import ModuleNotFoundIgnore
+from contextlib import suppress
 
-with ModuleNotFoundIgnore():
+with suppress(ModuleNotFoundError):
     from hum.gen.diagnosis_sounds import (
         WfGen, TimeSound, BinarySound, slow_mask, wf_with_timed_bleeps, mk_sounds_with_timed_bleeps
     )
 
-with ModuleNotFoundIgnore():
+with suppress(ModuleNotFoundError):
     from hum.gen.sine_mix import mk_sine_wf, freq_based_stationary_wf
 
-with ModuleNotFoundIgnore():
+with suppress(ModuleNotFoundError):
     from hum.gen.voiced_time import Voicer, tell_time_continuously
 
-with ModuleNotFoundIgnore():
-    from hum.gen.signal_generators import normal_dist, gen_words, categorical_gen, alphabet_to_bins, 
-    call_repeatedly, bernoulli, bernoulli_gen, inlier_outlier, signal, create_session, string_to_num, 
-    session_to_df
+with suppress(ModuleNotFoundError):
+    from hum.gen.signal_generators import (
+        normal_dist, gen_words, categorical_gen, alphabet_to_bins,
+        call_repeatedly, bernoulli, bernoulli_gen, inlier_outlier, signal, create_session, string_to_num,
+        session_to_df
+    )
