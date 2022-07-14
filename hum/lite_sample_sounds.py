f8bc0731f7b5eefefe04f8d573ffac8df8e5550f
Thor Whalen <thorwhalen1@gmail.com>
2022-07-13 17:15:46 -0700

    docs: tag_wf_gen cleaned, typed, docced and doctested

--- hum/lite_sample_sounds.py
+++ hum/lite_sample_sounds.py
@@ -4,7 +4,7 @@ Generate simple waveforms
 import numpy as np
 import random
 import itertools
-from typing import Mapping, Callable, Sequence, Any
+from typing import Mapping, Callable, Sequence, Any, Optional, Iterable, TypeVar
 from collections import defaultdict
 
 DFLT_SR = 44100
@@ -29,24 +29,17 @@ def chk_from_pattern(chk_size=DFLT_CHK_SIZE, pattern=None):
     ].astype(np.int16)
 
 
-def random_samples(
-    chk_size=DFLT_CHK_SIZE, max_amplitude=DFLT_MAX_AMPLITUDE, **kwargs
-):
+def random_samples(chk_size=DFLT_CHK_SIZE, max_amplitude=DFLT_MAX_AMPLITUDE, **kwargs):
     """
     Returns a random sample of integers of length chk_size in the range [-max_amplitude, max_amplitude]
     >>> np.random.seed(1)
     >>> assert all(random_samples(5) == [3003, -17828, -24808, 2511, 20057])
     """
-    return np.random.randint(-max_amplitude, max_amplitude, chk_size).astype(
-        np.int16
-    )
+    return np.random.randint(-max_amplitude, max_amplitude, chk_size).astype(np.int16)
 
 
 def pure_tone(
-    chk_size=DFLT_CHK_SIZE,
-    freq=440,
-    sr=DFLT_SR,
-    max_amplitude=DFLT_MAX_AMPLITUDE,
+    chk_size=DFLT_CHK_SIZE, freq=440, sr=DFLT_SR, max_amplitude=DFLT_MAX_AMPLITUDE,
 ):
     """
     Generates a pure tone using given arguments
@@ -59,10 +52,7 @@ def pure_tone(
 
 
 def triangular_tone(
-    chk_size=DFLT_CHK_SIZE,
-    freq=440,
-    sr=DFLT_SR,
-    max_amplitude=DFLT_MAX_AMPLITUDE,
+    chk_size=DFLT_CHK_SIZE, freq=440, sr=DFLT_SR, max_amplitude=DFLT_MAX_AMPLITUDE,
 ):
     """
     Generates a triangular tone using given arguments
@@ -75,10 +65,7 @@ def triangular_tone(
 
 
 def square_tone(
-    chk_size=DFLT_CHK_SIZE,
-    freq=440,
-    sr=DFLT_SR,
-    max_amplitude=DFLT_MAX_AMPLITUDE,
+    chk_size=DFLT_CHK_SIZE, freq=440, sr=DFLT_SR, max_amplitude=DFLT_MAX_AMPLITUDE,
 ):
     """
     Generates a square tone using given arguments
@@ -168,21 +155,68 @@ class AnnotatedWaveform(object):
         return np.array(wf), dict(slice_of_tag)
 
 
+Tag = TypeVar('Tag')  # TODO: Use generic for better typing
+
+
 def tag_wf_gen(
-    tag_wfgen_map: Mapping[Any, Callable[..., Sequence]] = tag_to_wf_gen_func_items,
-    tag_sequence=None,
+    tag_wfgen_map: Mapping[Tag, Callable[..., Sequence]] = tag_to_wf_gen_func_items,
+    tag_sequence: Optional[Iterable[Tag]] = None,
 ):
-    """Generate (tag, wf) pairs.
-    :param tag_wfgen_map: A {tag: wfgen, ...} map where wfgen is a callable taking no arguments and returning a sequence
+    """Generate tagged waveforms -- i.e. ``(tag, wf)`` pairs.
+
+    :param tag_wfgen_map: A ``{tag: wfgen, ...}`` map where wfgen is a callable taking
+        no arguments and returning a sequence
     :param tag_sequence: A sequence of tags (that should all be keys of tag_wfgen_map)
     :return:
+
+    Make a generator (here with default specs):
+
+    >>> gen = tag_wf_gen()
+
+    From it, source ``(tag, wf)`` pairs (by default, indefinitely).
+
+    >>> tag, wf = next(gen)
+
+    Since by default, we're given random ``tag`` and ``wf``, the only thing we can
+    verify here is that tag is a string and ``wf`` has the default size of 43008.
+
+    >>> type(tag)
+    <class 'str'>
+    >>> len(wf)
+    43008
+
+    By default the tags are randomly chosen from the ``tag_wfgen_map``, but you can
+    also specify a iterable of tags explicitly. Obviously, the elements of this
+    sequence must actually be keys of ``tag_wfgen_map`` -- this will be asserted if
+    the iterable is a Sequence.
+
+    You can specify a sequence of tags:
+
+    >>> gen = tag_wf_gen(
+    ...     tag_sequence=['random', 'pure_tone', 'triangular_tone', 'square_tone']
+    ... )
+    >>> tag_and_wf_seq = list(gen)  # consume the whole gen
+    >>> [x[0] for x in tag_and_wf_seq]
+    ['random', 'pure_tone', 'triangular_tone', 'square_tone']
+    >>> list(tag_and_wf_seq[2][1][:5])  # the first 5 numbers of the triangular tone
+    [-30000, -29900, -29800, -29700, -29600]
+
     """
-    tag_wfgen_map = dict(tag_to_wf_gen_func_items)
+    tag_wfgen_map = dict(tag_wfgen_map)
+
     if tag_sequence is None:
         indefinite_random_choice_from_tags = map(
             random.choice, itertools.repeat(list(tag_wfgen_map))
         )
         tag_sequence = indefinite_random_choice_from_tags
+    else:
+        if isinstance(tag_sequence, Sequence):
+            tag_sequence = list(tag_sequence)
+            if not all(tag in tag_wfgen_map for tag in tag_sequence):
+                raise ValueError(
+                    f'A tag_sequence must only have elements that are keys of '
+                    f'tag_wfgen_map, i.e. from {list(tag_wfgen_map)}'
+                )
 
     for tag in tag_sequence:
         yield tag, tag_wfgen_map[tag]()
